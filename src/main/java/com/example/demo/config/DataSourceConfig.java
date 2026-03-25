9d71d1a0bf0bc34419f126f68172254128bedfea
Tomoya Sasaki <tomoya.metropolis.part2@gmail.com>
2026-03-25 21:16:53 +0900

    Add bean for business datasource

--- src/main/java/com/example/demo/config/DataSourceConfig.java
+++ src/main/java/com/example/demo/config/DataSourceConfig.java
@@ -6,13 +6,21 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class DataSourceConfig {
 
-	@Bean
+	@Bean("dataSource")
+	@Primary
 	@ConfigurationProperties("spring.datasource")
 	DataSource dataSource() {
+		return DataSourceBuilder.create().build();
+	}
+
+	@Bean("businessDataSource")
+	@ConfigurationProperties("spring.business.datasource")
+	DataSource businessDataSource() {
 		return DataSourceBuilder.create().build();
 	}
 
