efb2a295f013441dfe94d96e2a3977f93b6d6b74
Tomoya Sasaki <tomoya.metropolis.part2@gmail.com>
2026-03-25 21:17:52 +0900

    Modify SQL to find members

--- src/main/java/com/example/demo/MemberStepListener.java
+++ src/main/java/com/example/demo/MemberStepListener.java
@@ -1,10 +1,14 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.jspecify.annotations.Nullable;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.listener.StepExecutionListener;
 import org.springframework.batch.core.step.StepExecution;
 import org.springframework.jdbc.core.JdbcOperations;
+
+import com.example.demo.domain.FullNameMember;
 
 public class MemberStepListener implements StepExecutionListener {
 
@@ -16,10 +20,11 @@ public class MemberStepListener implements StepExecutionListener {
 
 	@Override
 	public @Nullable ExitStatus afterStep(StepExecution stepExecution) {
-		String sql = "SELECT count(*) FROM member";
+		String sql = "SELECT id, first_name, last_name, full_name FROM member ORDER BY id";
 
-		int result = jdbcOperations.queryForObject(sql, Integer.class);
-		System.out.println("result = " + result);
+		List<FullNameMember> memberList = jdbcOperations.query(sql, (rs, rowNum) -> (new FullNameMember(rs.getInt("id"),
+				rs.getString("first_name"), rs.getString("last_name"), rs.getString("full_name"))));
+		memberList.forEach(System.out::println);
 
 		return StepExecutionListener.super.afterStep(stepExecution);
 	}
