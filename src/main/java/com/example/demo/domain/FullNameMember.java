fd50e44ef64768cc37b824605fc925c63a1fa4bd
t.sasaki <t.sasaki@future-shop.jp>
2026-04-13 14:47:24 +0900

    Use class instead of record

--- src/main/java/com/example/demo/domain/FullNameMember.java
+++ src/main/java/com/example/demo/domain/FullNameMember.java
@@ -1,4 +1,67 @@
 package com.example.demo.domain;
 
-public record FullNameMember(Integer id, String firstName, String lastName, String fullName) {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "member")
+public class FullNameMember {
+
+	@Id
+	@Column(name = "id")
+	private Integer id;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	@Column(name = "full_name")
+	private String fullName;
+
+	public FullNameMember() {
+	}
+
+	public FullNameMember(Integer id, String firstName, String lastName, String fullName) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.fullName = fullName;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
 }
