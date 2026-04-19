26dd679546a2fbd86ec1b67d11610f8330baf1c3
Tomoya Sasaki <tomoya.metropolis.part2@gmail.com>
2026-04-19 11:36:52 +0900

    Update BatchConfig with Kotlin

--- src/main/kotlin/com/example/demo/config/BatchConfig.kt
+++ src/main/kotlin/com/example/demo/config/BatchConfig.kt
@@ -1,11 +1,13 @@
 package com.example.demo.config
 
+import com.example.demo.domain.FullNameMember
 import com.example.demo.domain.Member
 import org.springframework.batch.core.job.Job
 import org.springframework.batch.core.job.builder.JobBuilder
 import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.core.step.Step
 import org.springframework.batch.core.step.builder.StepBuilder
+import org.springframework.batch.infrastructure.item.ItemProcessor
 import org.springframework.batch.infrastructure.item.ItemReader
 import org.springframework.batch.infrastructure.item.ItemWriter
 import org.springframework.batch.infrastructure.item.file.FlatFileItemReader
@@ -20,48 +22,47 @@ import org.springframework.validation.BindException
 @Configuration
 class BatchConfig {
 
-	@Bean
-	fun itemReader(): FlatFileItemReader<Member> {
-		return FlatFileItemReaderBuilder<Member>()
-			.name("memberItemReader")
-			.resource(ClassPathResource("bon_jovi.csv"))
-			.delimited { config -> config.delimiter(",").quoteCharacter('"').names("id", "first_name", "last_name") }
-			.fieldSetMapper(MemberMapper())
-			.linesToSkip(1)
-			.build()
-	}
+    @Bean
+    fun itemReader(): FlatFileItemReader<Member> =
+        FlatFileItemReaderBuilder<Member>().name("memberItemReader")
+            .resource(ClassPathResource("bon_jovi.csv"))
+            .delimited { config -> config.delimiter(",").quoteCharacter('"').names("id", "first_name", "last_name") }
+            .fieldSetMapper(MemberMapper())
+            .linesToSkip(1)
+            .build()
 
-	@Bean
-	fun itemWriter(): ItemWriter<Member> {
-		return ItemWriter { chunk ->
-			chunk.items.forEach { println(it) }
-		}
-	}
+    @Bean
+    fun itemProcessor(): ItemProcessor<Member, FullNameMember> =
+        ItemProcessor { item ->
+            FullNameMember(item.id, item.firstName, item.lastName, "${item.firstName} ${item.lastName}")
+        }
 
-	@Bean
-	fun step(jobRepository: JobRepository, itemReader: ItemReader<Member>, itemWriter: ItemWriter<Member>): Step {
-		return StepBuilder(jobRepository)
-			.chunk<Member, Member>(1)
-			.reader(itemReader)
-			.writer(itemWriter)
-			.build()
-	}
+    @Bean
+    fun itemWriter(): ItemWriter<FullNameMember> =
+        ItemWriter { chunk ->
+            chunk.items.forEach { println(it) }
+        }
 
-	@Bean
-	fun job(jobRepository: JobRepository, step: Step): Job {
-		return JobBuilder(jobRepository)
-			.start(step)
-			.build()
-	}
+    @Bean
+    fun step(
+        jobRepository: JobRepository,
+        itemReader: ItemReader<Member>,
+        itemProcessor: ItemProcessor<Member, FullNameMember>,
+        itemWriter: ItemWriter<FullNameMember>,
+    ): Step =
+        StepBuilder(jobRepository).chunk<Member, FullNameMember>(1)
+            .reader(itemReader)
+            .processor(itemProcessor)
+            .writer(itemWriter)
+            .build()
 
-	private class MemberMapper : FieldSetMapper<Member> {
-		@Throws(BindException::class)
-		override fun mapFieldSet(fieldSet: FieldSet): Member {
-			return Member(
-				fieldSet.readInt("id"),
-				fieldSet.readString("first_name")!!,
-				fieldSet.readString("last_name")!!
-			)
-		}
-	}
+    @Bean
+    fun job(jobRepository: JobRepository, step: Step): Job =
+        JobBuilder(jobRepository).start(step).build()
+
+    private class MemberMapper : FieldSetMapper<Member> {
+        @Throws(BindException::class)
+        override fun mapFieldSet(fieldSet: FieldSet): Member =
+            Member(fieldSet.readInt("id"), fieldSet.readString("first_name"), fieldSet.readString("last_name"))
+    }
 }
